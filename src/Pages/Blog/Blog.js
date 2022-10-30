1e3c3c3c6f7442c11974de5426589b31c8fbc722
sanudas2018 <sanu.das08103@gmail.com>
2022-10-30 22:09:34 +0600

    ADD question answer and FAQ

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -4,7 +4,85 @@ import "./Blog.css";
 const Blog = () => {
   return (
     <div>
-      <p>blog</p>
+      <div className="bolgContainer w-[90%] mx-auto mt-10 mb-[360px] bg-cyan-100 p-[20px]">
+        {/* 1 */}
+        <div
+          tabIndex={0}
+          className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box"
+        >
+          <div className="collapse-title text-xl font-medium">
+            what is cors?
+          </div>
+          <div className="collapse-content">
+            <p>
+              CORS is a node. js package for providing a Connect/Express
+              middleware that can be used to enable CORS with various options.
+              CORS acts as a middleware that allows the server side to use the
+              data
+            </p>
+          </div>
+        </div>
+        {/* 2 */}
+        <div
+          tabIndex={0}
+          className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box"
+        >
+          <div className="collapse-title text-xl font-medium">
+            Why are you using firebase? What other options do you have to
+            implement authentication?
+          </div>
+          <div className="collapse-content">
+            <p>
+              Firebase Authentication provides backend services, easy-to-use
+              SDKs, and ready-made UI libraries to authenticate users to your
+              app. It supports authentication using passwords, phone numbers,
+              popular federated identity providers like Google, Facebook and
+              Twitter, and more.Usually, authentication by a server entails the
+              use of a user name and password. Other ways to authenticate can be
+              through cards, retina scans, voice recognition, and fingerprints.
+            </p>
+          </div>
+        </div>
+        {/* 3 */}
+        <div
+          tabIndex={0}
+          className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box"
+        >
+          <div className="collapse-title text-xl font-medium">
+            How does the private route work?
+          </div>
+          <div className="collapse-content">
+            <p>
+              The private route component is similar to the public route, the
+              only change is that redirect URL and authenticate condition. If
+              the user is not authenticated he will be redirected to the login
+              page and the user can only access the authenticated routes If he
+              is authenticated (Logged in).
+            </p>
+          </div>
+        </div>
+        {/* 4 */}
+        <div
+          tabIndex={0}
+          className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box"
+        >
+          <div className="collapse-title text-xl font-medium">
+            What is Node? How does Node work?
+          </div>
+          <div className="collapse-content">
+            <p>
+              It is a used as backend service where javascript works on the
+              server-side of the application.Node.js is an open source server
+              environment Node.js is free Node.js runs on various platforms
+              (Windows, Linux, Unix, Mac OS X, etc.) This way javascript is used
+              on both frontend and backend. Node. js runs on chrome v8 engine
+              which converts javascript code into machine code, it is highly
+              scalable, lightweight, fast, and data-intensive.<br></br> Node.js
+              uses asynchronous programming!
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Pages/Faq/Faq.js
+++ src/Pages/Faq/Faq.js
@@ -4,7 +4,126 @@ import "./Faq.css";
 const Faq = () => {
   return (
     <div>
-      <p>faq</p>
+      <div className="collapseHome mt-10 mb-32">
+        <h2 className="text-3xl font-extrabold text-center text-indigo-500 mb-3">
+          Don't know some common questions and answers...
+        </h2>
+        <div className="totalCollapse grid grid-cols-1 lg:grid-cols-2 gap-6 w-[95%] mx-auto mt-9 bg-teal-100 p-10">
+          <div className="leftCol ">
+            <div
+              tabIndex={0}
+              className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box"
+            >
+              <div className="collapse-title text-xl font-medium">
+                I am new how can I do this
+              </div>
+              <div className="collapse-content">
+                <p>
+                  First of all welcome, if you want to do anything in
+                  freelancing or web sector first of all it is necessary to have
+                  a computer and internet connection. If you have everything
+                  ready, you can first watch our Day0 videos from YouTube, if
+                  you want, you can also learn from YouTube, as well as our Hand
+                  Holding Support program is also running, where you learn by
+                  holding through live classes from start to finish.
+                </p>
+              </div>
+            </div>
+            {/* 2- side  */}
+            <div
+              tabIndex={0}
+              className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box my-3"
+            >
+              <div className="collapse-title text-xl font-medium">
+                I only know HTML, CSS, so what do I do now?
+              </div>
+              <div className="collapse-content">
+                <p>
+                  What you start with will depend on your current activity and
+                  your needs. If your need is to earn very fast then WordPress
+                  is best for you, and if you are from CSE background or If you
+                  are passionate about programming and have enough time in your
+                  hands then I would say that starting with JavaScript or
+                  Laravel is the best.
+                </p>
+              </div>
+            </div>
+            {/* 3- slide  */}
+            <div
+              tabIndex={0}
+              className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box"
+            >
+              <div className="collapse-title text-xl font-medium">
+                Will Jhonkar Bhai take live classes?
+              </div>
+              <div className="collapse-content">
+                <p>
+                  It is not possible for me to take all the classes, but what
+                  happens is that I do not attend the classes of the marketplace
+                  myself and am connected with all the batches, meet and listen.
+                  The classes keep improving day by day.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="rightCol">
+            <div
+              tabIndex={0}
+              className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box"
+            >
+              <div className="collapse-title text-xl font-medium">
+                Do you have any courses?
+              </div>
+              <div className="collapse-content">
+                <p>
+                  All the courses we have till now Hand Holding Support For
+                  HTML(Day-0) to Advance React Hand Holding Support form
+                  WordPress(Day-35) Basic to Advance Full MERN Stack JavaScript
+                  Course Python with PHP
+                </p>
+              </div>
+            </div>
+            {/* 2- side  */}
+            <div
+              tabIndex={0}
+              className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box  my-3"
+            >
+              <div className="collapse-title text-xl font-medium">
+                What is market place support and how can I get it?
+              </div>
+              <div className="collapse-content">
+                <p>
+                  If you are a student of Hand Holding Support then you will get
+                  Lifetime Marketplace Support, In Market Place Support, your
+                  client's message review, whether to take a job or not to take
+                  a job, how to get a job, do not understand a client's
+                  requirement, with what plugging or how to do the overall
+                  support is provided from start to finish. But what is not
+                  done, you have got a task to do it yourself, our support team
+                  will support but not directly.
+                </p>
+              </div>
+            </div>
+            {/* 3- slide  */}
+            <div
+              tabIndex={0}
+              className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box"
+            >
+              <div className="collapse-title text-xl font-medium">
+                How many days will it take to complete the course?
+              </div>
+              <div className="collapse-content">
+                <p>
+                  If you enroll in our HTML to WordPress Advanced Course then it
+                  will take around 7-8 months to complete the course. And if you
+                  enroll in WordPress to advance course then it will take about
+                  5-6 months to complete the course.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
